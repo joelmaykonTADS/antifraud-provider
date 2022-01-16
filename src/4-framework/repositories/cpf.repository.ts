a254e3a3be959bb7f2137abc64b0faa75945703f
joel maykon <jmlima5@stefanini.com>
2022-01-16 13:53:51 -0300

    US1 create repository cpf

---
+++ src/4-framework/repositories/cpf.repository.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { InputCpfDto } from 'src/2-business/dto/cpf/input-cpf.dto';
+import { OutputCpfDto } from 'src/2-business/dto/cpf/output-cpf.dto';
+import { ICpfRepository } from 'src/2-business/repositories/cpf.respository.interfaces';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Cpf } from '../models/add-cpf.model';
+
+@Injectable()
+export class CpfRepository implements ICpfRepository {
+  constructor(@InjectModel('Cpf') private readonly cpfModel: Model<Cpf>) {}
+  public async save(cpf: InputCpfDto): Promise<OutputCpfDto> {
+    const addCpf = new this.cpfModel({
+      value: cpf.value,
+      createdAt: Date.now(),
+    });
+    return addCpf.save();
+  }
+}
