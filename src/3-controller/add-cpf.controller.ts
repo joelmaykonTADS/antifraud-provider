8e50ec74706f1572c0c956bf8e260151ac4de133
joel maykon <jmlima5@stefanini.com>
2022-01-16 13:54:50 -0300

    US1 controller to add cpf

---
+++ src/3-controller/add-cpf.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Param, Post } from '@nestjs/common';
+import { InputCpfDto } from '../2-business/dto/cpf/input-cpf.dto';
+import { OutputCpfDto } from '../2-business/dto/cpf/output-cpf.dto';
+import { AddCPFUseCase } from '../2-business/useCases/add-cpf.useCase';
+
+@Controller('cpf')
+export class AddCPFController {
+  constructor(private readonly addCpf: AddCPFUseCase) {}
+
+  @Post('/:cpf')
+  async run(@Param('cpf') cpf: string): Promise<OutputCpfDto> {
+    const input = new InputCpfDto();
+    input.value = cpf;
+    console.info(`init controller :: ${cpf}`);
+    const output = await this.addCpf.run(input);
+    console.info(`finished controller :: add cpf: ${JSON.stringify(output)}`);
+    return output;
+  }
+}
