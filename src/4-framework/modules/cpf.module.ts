3c8d01ee51785611878421a1e96ca0b1843eb4c3
joel maykon <jmlima5@stefanini.com>
2022-01-16 23:33:22 -0300

    US1 fix import

--- src/4-framework/modules/cpf.module.ts
+++ src/4-framework/modules/cpf.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AddCPFUseCase } from '../../../src/2-business/useCases/add-cpf.useCase';
-import { AddCPFController } from '../../../src/3-controller/add-cpf.controller';
+import { AddCPFUseCase } from 'src/2-business/useCases/add-cpf.useCase';
+import { AddCPFController } from 'src/3-controller/add-cpf.controller';
 import { CpfSchema } from '../models/add-cpf.model';
 import { CpfRepository } from '../repositories/cpf.repository';
 
