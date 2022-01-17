271c75ecdc7ca65dacbbcd0fab2f501a2765b7ec
joel maykon <jmlima5@stefanini.com>
2022-01-17 11:01:38 -0300

    US2 add injections module

--- src/4-framework/modules/cpf.module.ts
+++ src/4-framework/modules/cpf.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AddCPFUseCase } from 'src/2-business/useCases/add-cpf.useCase';
+import { CheckCPFUseCase } from 'src/2-business/useCases/check-cpf.useCase';
+import { ValidCPFUseCase } from 'src/2-business/useCases/valid-cpf.useCase';
 import { AddCPFController } from 'src/3-controller/add-cpf.controller';
-import { CpfSchema } from '../models/add-cpf.model';
+import { CheckCPFController } from 'src/3-controller/check-cpf.controller';
+import { CpfSchema } from '../models/cpf.model';
 import { CpfRepository } from '../repositories/cpf.repository';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'Cpf', schema: CpfSchema }])],
-  controllers: [AddCPFController],
+  controllers: [AddCPFController, CheckCPFController],
   providers: [
     AddCPFUseCase,
+    CheckCPFUseCase,
+    ValidCPFUseCase,
     {
       // You can switch useClass to different implementation
       useClass: CpfRepository,
