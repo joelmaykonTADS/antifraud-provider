24265a8d03a2991145ad3b878c26d2eded3a2adf
joel maykon <jmlima5@stefanini.com>
2022-01-16 23:11:57 -0300

    US1 add validations to save cpf

--- src/2-business/useCases/add-cpf.useCase.ts
+++ src/2-business/useCases/add-cpf.useCase.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { InputCpfDto } from '../dto/cpf/input-cpf.dto';
 import { OutputCpfDto } from '../dto/cpf/output-cpf.dto';
 import { ICpfRepository } from '../repositories/cpf.respository.interfaces';
@@ -12,7 +12,51 @@ export class AddCPFUseCase {
 
   async run(input: InputCpfDto): Promise<OutputCpfDto> {
     const cpf = new OutputCpfDto();
-    cpf.value = input.value;
-    return this._cpfRepository.save(cpf);
+    try {
+      cpf.value = this.formatted(input);
+      await this.isExist(cpf);
+      this.isRepeatedNumber(cpf);
+      this.isNotQuantityElevenDigits(cpf);
+      const createdCpf = this._cpfRepository.save(cpf);
+      return createdCpf;
+    } catch (e) {
+      console.log('error useCase add cpf');
+      return e;
+    }
+  }
+
+  private isRepeatedNumber(cpf: InputCpfDto): void {
+    const regex = /\b(\d)\1{10}\b/;
+    if (regex.test(cpf.value)) {
+      throw new BadRequestException({
+        type: 'InvalidCpfException',
+        message: 'CPF is not valid.',
+      });
+    }
+  }
+
+  private isNotQuantityElevenDigits(cpf: InputCpfDto): void {
+    const regex = /\b[\d]{11}\b/;
+    if (!regex.test(this.formatted(cpf))) {
+      throw new BadRequestException({
+        type: 'InvalidCpfException',
+        message: 'CPF is more than eleven digits.',
+      });
+    }
+  }
+
+  private formatted(cpf: InputCpfDto): string {
+    return cpf.value.replace(/\D+/g, '');
+  }
+
+  private async isExist(cpf: InputCpfDto): Promise<void> {
+    const result = await this._cpfRepository.findOne(cpf);
+    if (result) {
+      console.info(`Result cpf find one :: ${JSON.stringify(cpf)}`);
+      throw new BadRequestException({
+        type: 'ExistCpfException',
+        message: "CPF exist and can't use this numbers.",
+      });
+    }
   }
 }
--- test/units/2-business/useCases/add-cpf.useCase.spec.ts
+++ test/units/2-business/useCases/add-cpf.useCase.spec.ts
@@ -1,4 +1,6 @@
+import { BadRequestException } from '@nestjs/common';
 import { Model } from 'mongoose';
+import { NotFoundError } from 'rxjs';
 import { Cpf } from 'src/4-framework/models/add-cpf.model';
 import { InputCpfDto } from '../../../../src/2-business/dto/cpf/input-cpf.dto';
 import { OutputCpfDto } from '../../../../src/2-business/dto/cpf/output-cpf.dto';
@@ -14,20 +16,48 @@ describe('CPF UseCase', () => {
     repository = new CpfRepository(cpfModel);
     useCase = new AddCPFUseCase(repository);
   });
+
   test('Add CPF success', async () => {
     const date = new Date();
     const result = {
-      value: '123',
+      value: '00954817029',
       createdAt: date,
     };
+    jest.spyOn(repository, 'findOne').mockResolvedValue(null);
     jest.spyOn(repository, 'save').mockResolvedValue({
-      value: '123',
+      value: '00954817029',
       createdAt: date,
     } as OutputCpfDto);
     expect(
       await useCase.run({
-        value: '123',
+        value: '009.548.170-29',
       } as InputCpfDto),
     ).toEqual(result);
+  });
+
+  test('Try add CPF, error cpf exist', async () => {
+    const date = new Date();
+    jest.spyOn(repository, 'findOne').mockResolvedValue({
+      value: '00954817028',
+      createdAt: date,
+    } as OutputCpfDto);
+    expect(
+      await useCase.run({
+        value: '009.548.170-28',
+      } as InputCpfDto),
+    ).toBeInstanceOf(BadRequestException);
+  });
+
+  test('Try add CPF error, repeat numbers', async () => {
+    const date = new Date();
+    jest.spyOn(repository, 'findOne').mockResolvedValue({
+      value: '00954817029',
+      createdAt: date,
+    } as OutputCpfDto);
+    expect(
+      await useCase.run({
+        value: '009.548.170-29',
+      } as InputCpfDto),
+    ).toBeInstanceOf(BadRequestException);
   });
 });
